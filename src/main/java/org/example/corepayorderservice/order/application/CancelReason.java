6ddafe855f201db89352ecd902fc531520635d7c
jihoon-68 <pkrwlgns64@gmail.com>
2026-04-19 00:49:17 +0900

    feat(order): 결제 및 재고 복구 판단을 위한 CancelReason Enum 추가

---
+++ src/main/java/org/example/corepayorderservice/order/application/CancelReason.java
@@ -0,0 +1,22 @@
+package org.example.corepayorderservice.order.application;
+
+import lombok.Getter;
+
+@Getter
+public enum CancelReason {
+
+    OUT_OF_STOCK(false),
+
+    PRODUCT_NOT_FOUND(false),
+
+    CUSTOMER_CANCEL(true),
+
+    PAYMENT_FAILED(true);
+
+
+    private final boolean needStockRestore;
+
+    CancelReason(boolean needStockRestore) {
+        this.needStockRestore = needStockRestore;
+    }
+}
