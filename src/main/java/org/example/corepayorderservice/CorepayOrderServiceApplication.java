01323b837eb1b8c535a5cf4c45ef5446d99f2daa
jihoon-68 <pkrwlgns64@gmail.com>
2026-06-02 19:44:13 +0900

    chore: 불필요한 의존성 및 설정 제거

--- src/main/java/org/example/corepayorderservice/CorepayOrderServiceApplication.java
+++ src/main/java/org/example/corepayorderservice/CorepayOrderServiceApplication.java
@@ -2,11 +2,9 @@ package org.example.corepayorderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@EnableFeignClients
 @SpringBootApplication(scanBasePackages = {
         "org.example.corepayorderservice",
         "org.example.corepaycommon"
